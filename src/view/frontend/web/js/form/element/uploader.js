31e56f5d6e4c3a53e77a00915edf9bbbb5259491
C. M. de Picciotto <d3p1@d3p1.dev>
2026-05-05 21:02:25 +0000

    fix: adjust coding style guidelines [#1]

--- src/view/frontend/web/js/form/element/uploader.js
+++ src/view/frontend/web/js/form/element/uploader.js
@@ -2,22 +2,18 @@
  * @description Uploader JS
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-define([
-    'Magento_Ui/js/form/element/file-uploader'
-], function (Element) {
-    'use strict';
+define(['Magento_Ui/js/form/element/file-uploader'], function (Element) {
+    'use strict'
 
     return Element.extend({
         /**
-         *
          * @type {Object}
-         *
          */
         defaults: {
-            template      : 'D3p1_CustomerFile/form/element/uploader',
-            previewTmpl   : 'D3p1_CustomerFile/form/element/uploader/preview',
-            fieldScope    : 'customer',
-            fieldAttribute: ''
+            template: 'D3p1_CustomerFile/form/element/uploader',
+            previewTmpl: 'D3p1_CustomerFile/form/element/uploader/preview',
+            fieldScope: 'customer',
+            fieldAttribute: '',
         },
 
         /**
@@ -27,8 +23,8 @@ define([
          * @public
          */
         initialize: function () {
-            this._super();
-            this.fieldName = this.fieldScope + '[' + this.fieldAttribute + ']';
-        }
-    });
-});
+            this._super()
+            this.fieldName = this.fieldScope + '[' + this.fieldAttribute + ']'
+        },
+    })
+})
